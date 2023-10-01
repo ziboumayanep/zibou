9e5d571d5da5a9966c48500033f7545f82a08924
namnguyen <namnguyen107@gmail.com>
2023-10-01 22:47:04 +0200

    terraform: test ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,3 +14,7 @@ resource "google_service_account" "service_account" {
   account_id   = "cloudbuild"
   display_name = "Service Account"
 }
+
+resource "google_compute_global_address" "zibou" {
+  name = "zibou-ip2"
+}
