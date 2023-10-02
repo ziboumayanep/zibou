00a43c4edfe61e608ed9432234c1b4b32cbeb375
namnguyen <namnguyen107@gmail.com>
2023-10-02 15:53:51 +0200

    terraform: update secrets

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -2,6 +2,6 @@ locals {
   location            = "europe-west1"
   app_installation_id = "42100407"
   project_id          = "zibou-399608"
-  github_secret_id    = "github-connection-github-oauthtoken-79f143"
+  github_secret_id    = "github_token"
   github_user         = "ziboumima"
 }
