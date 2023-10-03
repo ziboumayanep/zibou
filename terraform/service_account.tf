d263f773f5ae7db15797409a49729275d5730c9f
namnguyen <namnguyen107@gmail.com>
2023-10-03 21:57:07 +0200

    tf: update location

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -1,6 +1,13 @@
 
 locals {
-  cloudbuild_permissions = toset(["roles/container.developer", "roles/iam.serviceAccountUser", "roles/logging.logWriter", "roles/storage.admin"])
+  cloudbuild_permissions = toset([
+    "roles/container.developer",
+    "roles/iam.serviceAccountUser",
+    "roles/logging.logWriter",
+    "roles/storage.admin",
+    "roles/artifactregistry.reader",
+    "roles/artifactregistry.writer"
+  ])
 }
 resource "google_service_account" "cloudbuild_service_account" {
   account_id   = "cloudbuild-sa"
