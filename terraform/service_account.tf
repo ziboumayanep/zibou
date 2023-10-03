798086b72896bb529136e9945bb874b1b4995d5a
namnguyen <namnguyen107@gmail.com>
2023-10-03 18:28:56 +0200

    update gke and builds

--- terraform/service_account.tf
+++ terraform/service_account.tf
@@ -1,23 +1,16 @@
 
+locals {
+  cloudbuild_permissions = toset(["roles/container.developer", "roles/iam.serviceAccountUser", "roles/logging.logWriter", "roles/storage.admin"])
+}
 resource "google_service_account" "cloudbuild_service_account" {
-  project      = var.project_id
   account_id   = "cloudbuild-sa"
   display_name = "Cloudbuild Service Account"
 }
 
-resource "google_project_iam_member" "cloudbuild" {
-  project = var.project_id
-  role    = "roles/container.developer" # cloudbuild sa needs this role to update gke
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
-}
-resource "google_project_iam_member" "act_as" {
-  project = var.project_id
-  role    = "roles/iam.serviceAccountUser"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
-}
 
-resource "google_project_iam_member" "logs_writer" {
-  project = var.project_id
-  role    = "roles/logging.logWriter"
-  member  = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
+resource "google_project_iam_member" "cloudbuild" {
+  for_each = local.cloudbuild_permissions
+  project  = var.project_id
+  role     = each.key
+  member   = "serviceAccount:${google_service_account.cloudbuild_service_account.email}"
 }
